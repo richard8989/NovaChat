163433d2b4c91004d90ce8e224ba19e8e7a06ea9
Ariel Perez <62973255+richard8989@users.noreply.github.com>
2020-12-03 04:43:28 -0500

    Add files via upload

---
+++ includes/leftbar.php
@@ -0,0 +1,14 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label">Main</li>
+			<li><a href="messages.php"><i class="fa fa-envelope"></i> &nbsp;Messages</a>
+			</li>
+      <li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
+			</li>
+			<li><a href="report.php"><i class="fa fa-bell"></i> &nbsp;Report User</a>
+			</li>			
+			</ul>
+	</nav>
+
+		
